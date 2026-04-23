dccfb6ac4180d1ffdbb9033076d5b92f478e4c51
Oleg Nechaev <oleg_nechaev_2019@mail.ru>
2026-04-23 13:56:08 +0300

    Добавил недостающий класс для будущих тестов

---
+++ src/Compiler/CommandLine/CompilerApplication.cs
@@ -0,0 +1,16 @@
+﻿namespace Compiler.CommandLine;
+
+/// <summary>
+/// Представляет консольное приложение для компилятора языка W.
+/// </summary>
+public static class CompilerApplication
+{
+    public static int Run(
+        string[] args,
+        TextWriter stdoutWriter,
+        TextWriter stderrWriter
+    )
+    {
+        return 0; // Пока что возвращаем 0
+    }
+}
